536614f5ad6cde78e5c96a5d8f0d53816ed30d98
jezair <max2777@ukr.net>
2025-09-16 13:13:45 +0300

    Add Nox + GitHub Actions CI

---
+++ noxfile.py
@@ -0,0 +1,14 @@
+import nox
+
+@nox.session(python=["3.10"])
+def tests(session):
+    """Запуск pytest"""
+    session.run("poetry", "install", external=True)
+    session.run("pytest", external=True)
+
+@nox.session(python=["3.10"])
+def lint(session):
+    """Линтинг через black и flake8"""
+    session.run("poetry", "install", external=True)
+    session.run("poetry", "run", "black", "--check", ".", external=True)
+    session.run("poetry", "run", "flake8", ".", external=True)
